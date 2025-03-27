b4eef3a0124a83eb9929dff3ad2e5b7489636069
swaqq-y <kir765x@gmail.com>
2025-03-27 14:08:33 +0300

    Пофиксил путь к файлу

--- launcher.py
+++ launcher.py
@@ -4,16 +4,24 @@ import subprocess
 safe_globals = {"__builtins__": None}
 safe_locals = {}
 class betterbutton:
+
     name = ''
     script = ''
-    code = open('./corescripts/scripts/repair_betterdiscord.py', 'r')
+    file = ''
+    btn = None
+    def __init__(self):
+        global name
+        global btn
+        btn = ttk.Button(text=name, command=self.runscript)
     def runscript(self):
-        global code
+        global file
+        code = open(file, 'r')
         exec(code, safe_globals, safe_locals)
 
 
 buttonslist = ['Repair BetterDiscord', 'Update ZapretDiscordYoutube', 'Download BetterDiscord Plugins', 'Download BetterDiscord Themes']
 listbutton = []
+
 
 
 # Создание интерфейса
