c483acf54edade0475908750c69e2d7d894f6329
swaqq-y <kirvl765@gmail.com>
2025-04-20 13:39:33 -0700

    Auto Update in dev

--- scripts/checkupdates.py
+++ scripts/checkupdates.py
@@ -1,28 +1,41 @@
 import requests
 import json
 from pathlib import Path
-import tkinter as tk
 from tkinter.messagebox import showinfo
-import shutil
-def get_github_repo_files():
-    url = f"https://api.github.com/repos/swaqq-y/Autolauncher/contents/"
-    headers = {
-        'Accept': 'application/vnd.github.v3+json'
-    }
-response = requests.get('https://raw.githubusercontent.com/swaqq-y/Autolauncher/main/settings.json')
+import zipfile
+import os
 
 config_path = Path(__file__).parent / 'settings.json'
 icon_path = Path(__file__).parent / 'icon.ico'
 
-githubconfig = json.loads(response.text)
-
 config = json.load(open(config_path))
+updatename = config['updatename']
+response = requests.get('https://raw.githubusercontent.com/swaqq-y/Autolauncher/{}/settings.json'.format(updatename))
 
-if githubconfig['version'] != config['version']:
-    config["version"] = githubconfig['version']
-    with open(config_path, 'w', encoding='utf-8') as file:
-        json.dump(config, file, ensure_ascii=False, indent=4)
 
-    showinfo('AutoLaucher Updates', 'AutoLaucher Updates are installed.')
-else:
-    showinfo('AutoLaucher Updates', 'Everything is up to date.')
+
+githubconfig = json.loads(response.text)
+currentfile = Path(__file__).resolve()
+
+
+#if githubconfig['version'] != config['version']:
+scriptsfiles = os.listdir('./')
+with zipfile.ZipFile("backup.zip", "w") as zf:
+    zf.write(currentfile.parent / "themes.txt")
+    zf.write(currentfile.parent / 'plugins.txt')
+    zf.write(currentfile.parent / 'settings.json')
+    zf.write(currentfile.parent / 'scripts.json')
+    zf.write(currentfile.parent / 'scripts/')
+    for i in scriptsfiles:
+        zf.write(currentfile.parent / 'scripts' / i)
+
+
+
+    # config["version"] = githubconfig['version']
+    # with open(config_path, 'w', encoding='utf-8') as file:
+    #     json.dump(config, file, ensure_ascii=False, indent=4)
+
+
+#     showinfo('AutoLauncher Updates', 'AutoLaucher Updates are installed.')
+# else:
+#     showinfo('AutoLauncher Updates', 'Everything is up to date.')
